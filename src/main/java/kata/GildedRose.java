a3fe412c0dd6160aa0bc5180a1e31b74952e3741
somkiat <somkiat.p@gmail.com>
2016-02-27 09:58:10 +0700

    Start of demo

---
+++ src/main/java/kata/GildedRose.java
@@ -0,0 +1,70 @@
+package kata;
+
+class GildedRose {
+
+    private String name;
+    private int quality;
+    private int daysRemaining;
+
+    public GildedRose(String name, int quality, int daysRemaining) {
+        this.name = name;
+        this.quality = quality;
+        this.daysRemaining = daysRemaining;
+    }
+
+    public void tick() {
+        if (name.equals("Aged Brie")
+                && !name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (quality > 0) {
+                if (!name.equals("Sulfuras, Hand of Ragnaros")) {
+                    quality -= 1;
+                }
+            }
+        } else {
+            if (quality < 50) {
+                quality += 1;
+
+                if (name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (daysRemaining < 11) {
+                        if (quality < 50) {
+                            quality += 1;
+                        }
+                    }
+
+                    if (daysRemaining < 6) {
+                        if (quality < 50) {
+                            quality += 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (!name.equals("Sulfuras, Hand of Ragnaros")) {
+            daysRemaining -= 1;
+        }
+
+        if (daysRemaining < 0) {
+            if (!name.equals("Aged Brie")) {
+                if (!name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (quality > 0) {
+                        if (!name.equals("Sulfuras, Hand of Ragnaros")) {
+                            quality -= 1;
+                        }
+                    }
+                } else {
+                    quality = 0;
+                }
+            } else {
+                if (quality < 50) {
+                    quality += 1;
+                }
+            }
+        }
+
+    }
+}
+
+
+
+
